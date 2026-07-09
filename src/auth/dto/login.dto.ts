67a2f7ab90310b727c94c4cc1e7172559ff58fc2
Sowhardo Bin Atik <105670032+Sowhardo12@users.noreply.github.com>
2026-07-09 14:18:06 +0600

    initialized Authentication process using Passport jwt in nestjs. Created login and register dto's for strict API data body checking. Created Auth Module, Service and controller and about to implement in next stage

---
+++ src/auth/dto/login.dto.ts
@@ -0,0 +1,9 @@
+import { IsEmail, IsString, MinLength } from "class-validator";
+export class LoginDto {
+  
+  @IsEmail({},{message:'Please Provide a valid email address'})
+  email!: string;
+  @IsString()
+  @MinLength(6,{message:'Password must be at least 6 characters long'})
+  password!: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  //input validation logic
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:true,
+      forbidNonWhitelisted:true,
+      transform:true,
+    })
+  )
+  //note: whitelist=true stripps down the unnecessary property 
+  //and forbindNonWhitelisted=true actually sends an error as response
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  console.log('server is listening on port 3000');
 }
 bootstrap();
