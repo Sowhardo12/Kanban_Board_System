c6ec0c8737d76cbecd605cdd6df2d8d856ff93c9
Sowhardo Bin Atik <105670032+Sowhardo12@users.noreply.github.com>
2026-07-09 15:35:45 +0600

    created AuthService functions: register and login. Register checks the DTO for already existing user, and if not, then registers with hashedPassword in user table. In case of login, user sends the email and password, and the password is compared with the hashed on stored in users row, and if ok, then an access_token is generated and sent as response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+import {JwtService} from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma : PrismaService,
+    private jwtService : JwtService,
+  ){}
+  //this.prisma = new PrismaService()
+  //this.jwtService = new JwtService()   nest does it
+  async register(dto: RegisterDto){
+    const existingUser = await this.prisma.user.findUnique(
+      {where:{email:dto.email},}
+    );
+    if(existingUser){
+      throw new ConflictException('Email is alreay registered');
+    }
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(dto.password,saltRounds);
+    const user = await this.prisma.user.create({
+      data:{
+        name:dto.name,
+        email:dto.email,
+        password:hashedPassword,
+      },
+    });
+    const {password,...others} = user;
+    return others;
+  }  
+
+  async login(dto:LoginDto){
+    //have to find user
+    const user = await this.prisma.user.findUnique({
+      where:{email:dto.email},
+    });
+    if(!user){
+      throw new UnauthorizedException('Invalid Credentials');
+    }
+    const isPasswordValid = await bcrypt.compare(dto.password,user.password);
+    if(!isPasswordValid){
+      throw new UnauthorizedException('Invalid Credentials');
+    }
+    const accessToken = this.jwtService.sign({sub:user.id,email:user.email});
+    return {
+      access_token : accessToken,
+    };
+  }
+}
